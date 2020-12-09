6a7396f4a6063369895cf0fdb789c9b14f67dbca
lauracb <lauracadavidb@gmail.com>
2020-12-08 22:15:25 -0500

    Se genera el manejador de buscar, ver imagen, actualizar.

--- Project/app.py
+++ Project/app.py
@@ -11,7 +11,8 @@ import os
 # secret_key=os.urandom(24)
 # secret_key='secrect123'
 yag = yagmail.SMTP('misiontic2022grupo11@gmail.com', '2022Grupo11')
-# UPLOAD_FOLDER = 'D:\Descarga\Mintic\Ciclo3\ProyectoGrupoE'
+#UPLOAD_FOLDER = 'D:\Descarga\Mintic\Ciclo3\ProyectoGrupoE'
+UPLOAD_FOLDER = 'C:\Descargas'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
@@ -22,7 +23,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #Laura
 
 #Check the session at the beggining of request
-
 @app.before_request
 def before_request():
     g.user = None
@@ -30,7 +30,7 @@ def before_request():
         user = [x for x in users if x.id == session['user_id']][0]
         g.user = user
 
-#Classes of LoginForm
+#Classes of Login
 class LoginForm(Form):
     user = StringField('Usuario',[
         validators.Length(min=1, max=50, message=('El nombre debe tener máximo 50 caracteres')),
@@ -57,8 +57,7 @@ users.append(User(id=2, username='Jose', password='secret'))
 users.append(User(id=3, username='Luis', password='hola'))
 users.append(User(id=4, username='Ivan', password='mundo'))
 ##
-
-#End Classes of LoginForm
+#End Classes of Login
 
 # Login Route
 @app.route('/login', methods=['GET', 'POST'])
@@ -97,6 +96,22 @@ def log_out():
     return redirect(url_for('index'))
 # End Logout Route
 
+# Update Route
+@app.route('/update')
+def update():
+    if not g.user:
+        return redirect(url_for('login'))
+    return render_template('UpdateView/update.html')
+# End Update Route
+
+# DeleteImage Route
+@app.route('/imageDelete/<string:id>', methods=["POST"]) 
+def image_delete(id):
+    if not g.user:
+        return redirect(url_for('login'))
+    return redirect(url_for('update'))
+# End DeleteImage Route
+
 # Search Route
 @app.route('/search/<string:name>')
 def search_image(name):
@@ -104,8 +119,8 @@ def search_image(name):
 #End Search Route
 
 # ShowImage Route
-@app.route('/showImage')
-def showImage():
+@app.route('/showImage/<string:id>')
+def showImage(id):
     return render_template('ShowImage/showImage.html')
 #End ShowImage Route
 
@@ -126,20 +141,7 @@ def download():
     return status
 # End Download Route
 
-
-# Update Route
-@app.route('/update')
-def update():
-    return render_template('UpdateView/update.html')
-# End Update Route
-
-# DeleteImage Route
-@app.route('/imageDelete/<id>', methods=["DELETE"])
-def image_delete(id):
-    return redirect(url_for('update'))
-# End DeleteImage Route
-
-# ResetRequest Route
+# ResetRequest Route (Validar que no esté repetida)
 @app.route('/resetRequest', methods=['GET', 'POST'])
 def resetRequest():
     return render_template('Reset/resetRequest.html')
@@ -233,6 +235,8 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    if not g.user:
+        return redirect(url_for('login'))
     form = UploadForm(request.form)
     if request.method == 'POST' and form.validate():
         title = form.title.data
